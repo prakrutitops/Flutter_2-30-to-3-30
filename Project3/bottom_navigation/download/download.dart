f54fdc1b77b953881c05e009792d949d1ea0ef0e
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-07-04 15:24:33 +0530

    Add files via upload

---
+++ Project3/bottom_navigation/download/download.dart
@@ -0,0 +1,30 @@
+//@dart=2.9
+import 'dart:async';
+import 'dart:io';
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+import '../../../constants.dart';
+
+class Download extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return DownloadPage();
+  }
+}
+
+class DownloadPage extends State<Download> {
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return Scaffold(
+
+    );
+  }
+}
---
+++ Project3/bottom_navigation/home/home.dart
@@ -0,0 +1,32 @@
+//@dart=2.9
+import 'dart:async';
+import 'dart:io';
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+import '../../../constants.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return HomePage();
+  }
+}
+
+class HomePage extends State<Home> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return Scaffold(
+
+    );
+  }
+}
