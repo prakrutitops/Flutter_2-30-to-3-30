88d0a8fcd33d491ba8161a1ce60426e9eb19e903
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-07-08 15:54:00 +0530

    Add files via upload

---
+++ project5/user/front/front.dart
@@ -0,0 +1,99 @@
+//@dart=2.9
+
+import 'package:flutter/material.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../constants.dart';
+
+import '../bottom_navigation/download/download.dart';
+import '../bottom_navigation/home/home.dart';
+import '../login/login.dart';
+
+class Front extends StatefulWidget
+{
+  @override
+  FrontPage createState() => FrontPage();
+}
+
+class FrontPage extends State<Front>
+{
+  SharedPreferences logindata;
+  String username;
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>
+  [
+    Home(),
+    Download(),
+  ];
+
+  void _onItemTapped(int index)
+  {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initial();
+  }
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('E wishes Welcome :$username', style: TextStyle(color: kGold),),
+        automaticallyImplyLeading: false,
+        backgroundColor: kDarkBrown,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.logout,
+              color: kGold,
+            ),
+            onPressed: () {
+
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login()));
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      backgroundColor: kLightGold,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.download),
+              label: 'Download'
+            //title: Text('Download'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: kGold,
+        backgroundColor: kDarkBrown,
+        unselectedItemColor: kTerracotta,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+  void initial()async
+  {
+
+    logindata = await SharedPreferences.getInstance();
+    setState(() {
+      username = logindata.getString('username');
+    });
+
+
+  }
+}
---
+++ project5/user/splashscreen/splashscreen.dart
@@ -0,0 +1,101 @@
+//@dart=2.9
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:connectivity/connectivity.dart';
+
+import '../../constants.dart';
+import '../onboarding/onboarding.dart';
+
+class SplashScreen extends StatefulWidget
+{
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen>
+{
+
+  @override
+  void initState()
+  {
+    // TODO: implement initState
+   //
+
+    checkConnection();
+    super.initState();
+
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+      return Scaffold
+        (
+          backgroundColor: kLightGold,
+            body: Center(
+
+                child: Image
+                  (
+                    image: AssetImage("images/logo.png"),
+                    height: MediaQuery.of(context).size.height - 360,
+                    width: MediaQuery.of(context).size.width - 330
+                 ),
+
+
+            )
+          
+
+        );
+  }
+
+  void checkConnection() async
+  {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+
+    if (connectivityResult == ConnectivityResult.mobile)
+    {
+      check_if_already_login();
+    }
+    else if (connectivityResult == ConnectivityResult.wifi)
+    {
+      check_if_already_login();
+    }
+    else
+    {
+      _showConnectionDialog();
+    }
+  }
+
+  void check_if_already_login()
+  {
+
+    Timer(Duration(seconds: 3), ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Onboarding())),);
+
+  }
+  void _showConnectionDialog() async
+  {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: kGrey,
+          title: Row(children: [Icon(Icons.error),Text("\t Network Error")],),
+          content: Text('Please check your internet connection.',style: TextStyle(fontStyle: FontStyle.italic)),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: ()
+              {
+                exit(0);
+              },
+              child: Text("Exit",style: TextStyle(color: Colors.grey)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
